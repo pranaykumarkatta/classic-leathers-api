c6e6a32d860d0a781ac0df9945ed551fab30e9e0
Pranay <pranay.katta1@gmail.com>
2023-08-20 12:04:51 +0530

    Changes to add daily sales graph

--- src/main/java/com/classicLeathers/classicLeathersTool/retail/model/TotalSalesDto.java
+++ src/main/java/com/classicLeathers/classicLeathersTool/retail/model/TotalSalesDto.java
@@ -3,12 +3,19 @@ package com.classicLeathers.classicLeathersTool.retail.model;
 public class TotalSalesDto {
     private String month;
     private Integer drivingSales;
+    private Integer miratSales;
+    private Integer otherFormalsSales;
+    private Integer otherCasualsSales;
+    private Integer catSales;
     private Integer koraSales;
     private Integer wavesSales;
     private Integer aSeriesSales;
-    private Integer miratSales;
+    private Integer justonSales;
+    private Integer crocsSales;
     private Integer handBagSales;
     private Integer beltsAndWalletsSale;
+    private Integer serviceSales;
+    private Integer ottoSales;
     private Integer naSalesSale;
 
     public String getMonth() {
@@ -20,15 +27,47 @@ public class TotalSalesDto {
     }
 
     public Integer getDrivingSales() {
-        return drivingSales;
+        return drivingSales == null ? 0 : drivingSales;
     }
 
     public void setDrivingSales(Integer drivingSales) {
         this.drivingSales = drivingSales;
     }
 
+    public Integer getMiratSales() {
+        return miratSales == null ? 0 : miratSales;
+    }
+
+    public void setMiratSales(Integer miratSales) {
+        this.miratSales = miratSales;
+    }
+
+    public Integer getOtherFormalsSales() {
+        return otherFormalsSales == null ? 0 : otherFormalsSales;
+    }
+
+    public void setOtherFormalsSales(Integer otherFormalsSales) {
+        this.otherFormalsSales = otherFormalsSales;
+    }
+
+    public Integer getOtherCasualsSales() {
+        return otherCasualsSales == null ? 0 : otherCasualsSales;
+    }
+
+    public void setOtherCasualsSales(Integer otherCasualsSales) {
+        this.otherCasualsSales = otherCasualsSales;
+    }
+
+    public Integer getCatSales() {
+        return catSales == null ? 0 : catSales;
+    }
+
+    public void setCatSales(Integer catSales) {
+        this.catSales = catSales;
+    }
+
     public Integer getKoraSales() {
-        return koraSales;
+        return koraSales == null ? 0 : koraSales;
     }
 
     public void setKoraSales(Integer koraSales) {
@@ -36,7 +75,7 @@ public class TotalSalesDto {
     }
 
     public Integer getWavesSales() {
-        return wavesSales;
+        return wavesSales == null ? 0 : wavesSales;
     }
 
     public void setWavesSales(Integer wavesSales) {
@@ -44,23 +83,31 @@ public class TotalSalesDto {
     }
 
     public Integer getaSeriesSales() {
-        return aSeriesSales;
+        return aSeriesSales == null ? 0 : aSeriesSales;
     }
 
     public void setaSeriesSales(Integer aSeriesSales) {
         this.aSeriesSales = aSeriesSales;
     }
 
-    public Integer getMiratSales() {
-        return miratSales;
+    public Integer getJustonSales() {
+        return justonSales == null ? 0 : justonSales;
     }
 
-    public void setMiratSales(Integer miratSales) {
-        this.miratSales = miratSales;
+    public void setJustonSales(Integer justonSales) {
+        this.justonSales = justonSales;
+    }
+
+    public Integer getCrocsSales() {
+        return crocsSales == null ? 0 : crocsSales;
+    }
+
+    public void setCrocsSales(Integer crocsSales) {
+        this.crocsSales = crocsSales;
     }
 
     public Integer getHandBagSales() {
-        return handBagSales;
+        return handBagSales == null ? 0 : handBagSales;
     }
 
     public void setHandBagSales(Integer handBagSales) {
@@ -68,15 +115,31 @@ public class TotalSalesDto {
     }
 
     public Integer getBeltsAndWalletsSale() {
-        return beltsAndWalletsSale;
+        return beltsAndWalletsSale == null ? 0 : beltsAndWalletsSale;
     }
 
     public void setBeltsAndWalletsSale(Integer beltsAndWalletsSale) {
         this.beltsAndWalletsSale = beltsAndWalletsSale;
     }
 
+    public Integer getOttoSales() {
+        return ottoSales == null ? 0 : ottoSales;
+    }
+
+    public void setOttoSales(Integer ottoSales) {
+        this.ottoSales = ottoSales;
+    }
+
+    public Integer getServiceSales() {
+        return serviceSales == null ? 0 : serviceSales;
+    }
+
+    public void setServiceSales(Integer serviceSales) {
+        this.serviceSales = serviceSales;
+    }
+
     public Integer getNaSalesSale() {
-        return naSalesSale;
+        return naSalesSale == null ? 0 : naSalesSale;
     }
 
     public void setNaSalesSale(Integer naSalesSale) {
