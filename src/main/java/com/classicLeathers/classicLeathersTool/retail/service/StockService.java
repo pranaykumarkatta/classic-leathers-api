09d89bec5f5eb8ea8f8f0a033e3d16aeeb14beac
Pranay <pranay.katta1@gmail.com>
2024-10-02 07:54:01 +0530

    CODE FOR ONLINE STOCK SYNC FILE

--- src/main/java/com/classicLeathers/classicLeathersTool/retail/service/StockService.java
+++ src/main/java/com/classicLeathers/classicLeathersTool/retail/service/StockService.java
@@ -184,9 +184,15 @@ public class StockService {
                     new FileUtils().WriteData("D:\\onedrive\\CLASSIC_DOCS\\RETAIL_DOCS\\2024\\STOCK_AUDIT.xlsx", 0, data);
                 } else {
                     new FileUtils().WriteData("D:\\onedrive\\CLASSIC_DOCS\\RETAIL_DOCS\\2024\\STOCK_AUDIT_REPORT.xlsx", 0, data);
-                    if (getOnlineSkus().contains(dto.getSku()) && (!dto.getTo().equals("MYNTRA"))) {
+                    if (getOnlineSkus().contains(dto.getSku()) && (!dto.getTo().equals("MYNTRA")) && (!dto.getFrom().equals("CLASSIC LEATHERS RETAIL"))){
                         Object[] d1 = new Object[]{
                                 dto.getSku(), 1, "DEFAULT", "REMOVE"
+                        };
+                        new FileUtils().WriteData("D:\\onedrive\\CLASSIC_DOCS\\RETAIL_DOCS\\2024\\INVENTORY_ADJUSTMENT.xlsx", 0, d1);
+                    }
+                    if (getOnlineSkus().contains(dto.getSku()) && (!dto.getTo().equals("CLASSIC LEATHERS RETAIL"))){
+                        Object[] d1 = new Object[]{
+                                dto.getSku(), 1, "DEFAULT", "ADD"
                         };
                         new FileUtils().WriteData("D:\\onedrive\\CLASSIC_DOCS\\RETAIL_DOCS\\2024\\INVENTORY_ADJUSTMENT.xlsx", 0, d1);
                     }
